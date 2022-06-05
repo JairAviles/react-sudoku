4fd995235e9d2b55b12201c44ea6495f576e8319
Jair Aviles <jair.aviles@segment.com>
2022-06-05 14:13:25 -0700

    Add base URL on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://jairaviles.github.io/',
   plugins: [react(), tsconfigPaths()],
   server: {
     port: 3000
